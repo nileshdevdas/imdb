9c8de484abb188c2b68b08e78147261d6193e681
Nilesh Devdas <nildev@email.com>
2016-12-08 11:55:33 +0530

    Commit by nilesh

--- app/login.service.ts
+++ app/login.service.ts
@@ -1,15 +1,15 @@
 import {Injectable } from '@angular/core';
-import {Subject} from 'rxjs/Subject';
+import {Subject} from 'rxjs/Subject'; //1
 import { 
          SessionStorage, 
          SessionStorageService, 
        } from 'ng2-webstorage';
 @Injectable()
 export class LoggedService {
-    private loginSource =new Subject<string>();
-    private logoutSource =new Subject<string>();
-    loginBroadcaster = this.loginSource.asObservable();
-    logoutBroadcaster = this.logoutSource.asObservable();
+    private loginSource =new Subject<string>();  // 2
+    private logoutSource =new Subject<string>(); // 2
+    loginBroadcaster = this.loginSource.asObservable(); //3
+    logoutBroadcaster = this.logoutSource.asObservable();//3
     @SessionStorage()
     islogged: boolean;
     @SessionStorage()
@@ -17,13 +17,13 @@ export class LoggedService {
     login(username: string): boolean {
         this.islogged = true;
         this.logged = username;
-        this.loginSource.next(username);
+        this.loginSource.next(username); //4 
         return this.islogged;
     }
     logout(username: string): boolean {
         this.islogged = false;
         this.logged= undefined;
-        this.logoutSource.next(username);
+        this.logoutSource.next(username); //4 
         return this.islogged;
     }
 }
